ebedad405767f2f11c026b162116ffefed9ab30e
Tony Young <tony@rfw.name>
2012-07-11 13:48:39 +1200

    Add support for more operations, as well as add support for certain native operations (reduces).
    The new operations are sort, max, min, take, skip, takeWhile, skipWhile and
    concat.

--- sloth.js
+++ sloth.js
@@ -4,15 +4,12 @@
     /*
      * sloth.js is a JavaScript library for working with lazy iterators,
      * providing a way to create, compose and perform various other operations
-     * on them.
+     * on them --- forming a composable algebra of operations on iterators.
      *
      * A lazy iterator in sloth.js is defined as a function (usually a closure)
      * which can be continually invoked to yield successive values of a
      * sequence, until the appropriate exception, `sloth.StopIteration`, is
      * thrown to indicate the end of the sequence.
-     *
-     * TODO: Implement `take(n)`, `skip(n)`, `takeWhile(f)`, `skipWhile(f)`,
-     *       `max(f)`, `min(f)`; unit tests for `all(f)` and `any(f)`.
      */
     var sloth = {
         /*
@@ -33,14 +30,71 @@
         },
 
         /*
-         * Create an iterator from an array.
+         * The default comparison function, combining both lexicographic and
+         * numerical semantics.
+         */
+        cmp: function(a, b) {
+            return a < b ? -1 :
+                   a > b ? 1 :
+                   0;
+        },
+
+        /*
+         * Create an iterator for an array. Note that this is the low-level
+         * iterator and needs to be `sloth.wrap`ped for useful things.
          */
         iterArray: function(array) {
-            var index = 0;
+            var i = 0;
 
             return function() {
-                if(index >= array.length) throw sloth.StopIteration;
-                return array[index++];
+                if(i >= array.length) throw sloth.StopIteration;
+                return array[i++];
+            };
+        },
+
+        /*
+         * Create an iterator for a string.  Note that this is the low-level
+         * iterator and needs to be `sloth.wrap`ped for useful things.
+         */
+        iterString: function(string) {
+            var i = 0;
+
+            return function() {
+                if(i >= string.length) throw sloth.StopIteration;
+                return string.charAt(i++);
+            };
+        },
+
+        /*
+         * Create an iterator for a range. Comes in three variants:
+         *
+         * * If only a single argument `a` is provided, an iterator for a range
+         *   from 0 to `a - 1` by a step of 1.
+         *
+         * * If the third argument, `step`, is left unspecified, the step
+         *   defaults to 1.
+         *
+         * * If all three arguments are specified, a range from `a` to `b - 1`
+         *   is created by a step of `step`.
+         *
+         * Note that the end of the range does not include `b` --- this is
+         * influenced by Python's `range` function.
+         *
+         * Also note that this is the low-level iterator and needs to be
+         * `sloth.wrap`ped for useful things.
+         */
+        iterRange: function(a, b, step) {
+            if(arguments.length == 1) {
+                b = a;
+                a = 0;
+                step = 1;
+            } else if(typeof step === "undefined") {
+                step = 1;
+            }
+
+            return function() {
+                if(a >= b) throw sloth.StopIteration;
+                return (a += step) - step;
             };
         },
 
@@ -54,7 +108,7 @@
                 /*
                  * `map` applies a function across all elements of a sequence.
                  *
-                 * This is a lazy operation.
+                 * This is a lazy, composable operation.
                  */
                 map: function(f) {
                     return sloth.wrap(function() {
@@ -66,7 +120,7 @@
                  * `filter` selects elements from a sequence that are `true`
                  * when the predicate is applied to them.
                  *
-                 * This is a lazy operation.
+                 * This is a lazy, composable operation.
                  */
                 filter: function(f) {
                     return sloth.wrap(function() {
@@ -84,14 +138,14 @@
                  * `sloth.StopIteration` can be thrown at any time to break out
                  * of the loop.
                  *
-                 * This is a strict operation.
+                 * This is a strict, non-composable operation.
                  */
                 each: function(f) {
                     for(;;) {
                         try {
                             var value = iter();
                         } catch(e) {
-                            if(e == sloth.StopIteration) break;
+                            if(e === sloth.StopIteration) break;
                             throw e;
                         }
                         f(value);
@@ -102,7 +156,7 @@
                  * `force` gets all the elements from the iterator and places
                  * them into an array.
                  *
-                 * This is a strict operation.
+                 * This is a strict, non-composable operation.
                  */
                 force: function() {
                     var output = [];
@@ -115,8 +169,7 @@
                 /*
                  * `reverse` reverses the underlying iterator.
                  *
-                 * This is a "lazy" operation that requires strict (i.e.
-                 * non-infinite) iterator semantics.
+                 * This is a strict, composable operation.
                  */
                 reverse: function() {
                     var array = sloth.wrap(iter).force();
@@ -125,13 +178,36 @@
                 },
 
                 /*
+                 * `sort` sorts an array using a comparison function.
+                 *
+                 * This is a strict, composable operation.
+                 */
+                sort: function(f) {
+                    if(typeof f === "undefined") f = sloth.cmp;
+
+                    var array = sloth.wrap(iter).force();
+                    array.sort(f);
+                    return sloth.wrap(sloth.iterArray(array));
+                },
+
+                /*
                  * `foldl` is an implementation of the left-fold operation,
                  * also known as a left-reduce or inject.
                  *
-                 * This is a strict operation.
+                 * This is a strict, non-composable operation.
                  */
                 foldl: function(f, acc) {
-                    if(typeof acc === 'undefined') acc = iter();
+                    if(Array.prototype.reduce) {
+                        // We can delegate to the native
+                        // Array.prototype.reduce, if it's supported.
+                        var array = sloth.wrap(iter).force();
+                        return arguments.length == 1 ?
+                               Array.prototype.reduce.call(array, f) :
+                               Array.prototype.reduce.call(array, f, acc);
+                    }
+
+                    if(arguments.length == 1) acc = iter();
+
                     sloth.wrap(iter).each(function(x) {
                         acc = f(acc, x);
                     });
@@ -142,12 +218,22 @@
                  * `foldr` is an implementation of the right-fold operation,
                  * the reverse analog of `foldl` operation.
                  *
-                 * This is a strict operation.
+                 * This is a strict, non-composable operation.
                  */
                 foldr: function(f, acc) {
+                    // We can delegate to the native
+                    // Array.prototype.reduceRight, if it's supported.
+                    if(Array.prototype.reduceRight) {
+                        var array = sloth.wrap(iter).force();
+                        return arguments.length == 1 ?
+                               Array.prototype.reduceRight.call(array, f) :
+                               Array.prototype.reduceRight.call(array, f, acc);
+                    }
+
                     var reverseIter = sloth.wrap(iter).reverse().next;
 
-                    if(typeof acc === 'undefined') acc = reverseIter();
+                    if(arguments.length == 1) acc = reverseIter();
+
                     sloth.wrap(reverseIter).each(function(x) {
                         acc = f(acc, x);
                     });
@@ -158,7 +244,7 @@
                  * `all` checks if all values in the sequence are truthy or
                  * fulfill the predicate (universal quantification).
                  *
-                 * This is a strict operation.
+                 * This is a partially strict non-composable operation.
                  */
                 all: function(f) {
                     if(typeof f === "undefined") f = sloth.id;
@@ -167,7 +253,7 @@
                         try {
                             var value = iter();
                         } catch(e) {
-                            if(e == sloth.StopIteration) break;
+                            if(e === sloth.StopIteration) break;
                             throw e;
                         }
                         if(!f(value)) return false;
@@ -179,7 +265,7 @@
                  * `any` checks if any values in the sequence are truthy or
                  * fulfill the predicate (existential quantification).
                  *
-                 * This is a strict operation.
+                 * This is a partially strict non-composable operation.
                  */
                 any: function(f) {
                     if(typeof f === "undefined") f = sloth.id;
@@ -188,12 +274,131 @@
                         try {
                             var value = iter();
                         } catch(e) {
-                            if(e == sloth.StopIteration) break;
+                            if(e === sloth.StopIteration) break;
                             throw e;
                         }
                         if(f(value)) return true;
                     }
                     return false;
+                },
+
+                /*
+                 * `max` returns the maximum value of the sequence using a
+                 * given comparison function.
+                 *
+                 * This is a strict, non-composable operation.
+                 */
+                max: function(f) {
+                    if(typeof f === "undefined") f = sloth.cmp;
+
+                    return sloth.wrap(iter).foldl(function(acc, x) {
+                        return f(acc, x) > 0 ? acc : x;
+                    });
+                },
+
+                /*
+                 * `max` returns the maximum value of the sequence using a
+                 * given comparison function.
+                 *
+                 * This is a strict, non-composable operation.
+                 */
+                min: function(f) {
+                    if(typeof f === "undefined") f = sloth.cmp;
+
+                    return sloth.wrap(iter).foldl(function(acc, x) {
+                        return f(acc, x) < 0 ? acc : x;
+                    });
+                },
+
+                /*
+                 * `take` yields a sequence with only the first _n_ elements of
+                 * the original sequence.
+                 *
+                 * This is a lazy, composable operation.
+                 */
+                take: function(n) {
+                    return sloth.wrap(function() {
+                        if(n == 0) throw sloth.StopIteration;
+                        n--;
+                        return iter();
+                    });
+                },
+
+                /*
+                 * `skip` yields a sequence without the first _n_ elements of
+                 * the original sequence.
+                 *
+                 * This is a lazy, composable operation.
+                 */
+                skip: function(n) {
+                    return sloth.wrap(function() {
+                        while(n) {
+                            iter();
+                            n--;
+                        }
+                        return iter();
+                    });
+                },
+
+                /*
+                 * `takeWhile` yields a sequence with only the first
+                 * contiguous sequence of elements that fulfill the predicate.
+                 *
+                 * This is a lazy, composable operation.
+                 */
+                takeWhile: function(f) {
+                    var ended = false;
+
+                    return sloth.wrap(function() {
+                        var value = iter();
+                        ended = !f(value);
+                        if(ended) throw sloth.StopIteration;
+                        return value;
+                    });
+                },
+
+                /*
+                 * `skipWhile` yields a sequence without the first
+                 * contiguous sequence of elements that fulfill the predicate.
+                 *
+                 * This is a lazy, composable operation.
+                 */
+                skipWhile: function(f) {
+                    var value;
+                    while(f(value = iter()));
+
+                    var firstTaken = false;
+
+                    return sloth.wrap(function() {
+                        if(firstTaken) return iter();
+                        firstTaken = true;
+                        return value;
+                    });
+                },
+
+                /*
+                 * `concat` joins two sequences to each other, end-to-end. Note
+                 * that the `ys` parameter must be an actual iterator and not a
+                 * `sloth.wrap`ped iterator (this can be taken from any
+                 * `sloth.wrap`ped iterator with `wrapped.next`).
+                 *
+                 * This is as lazy, composable operation.
+                 */
+                concat: function(ys) {
+                    var xsEnd = false;
+
+                    return sloth.wrap(function() {
+                        if(xsEnd) return ys();
+                        try {
+                            return iter();
+                        } catch(e) {
+                            if(e === sloth.StopIteration) {
+                                xsEnd = true;
+                                return ys();
+                            }
+                            throw e;
+                        }
+                    });
                 },
 
                 /*
--- tests.js
+++ tests.js
@@ -16,6 +16,80 @@ module.exports = {
         test.done();
     },
 
+    testIterString: function(test) {
+        test.expect(5);
+        var iter = sloth.iterString("test");
+        test.strictEqual("t", iter());
+        test.strictEqual("e", iter());
+        test.strictEqual("s", iter());
+        test.strictEqual("t", iter());
+        try {
+            iter();
+        } catch(e) {
+            test.strictEqual(sloth.StopIteration, e);
+        }
+        test.done();
+    },
+
+    testCmpNumber: function(test) {
+        test.expect(3);
+        test.ok(sloth.cmp(2, 10) < 0);
+        test.ok(sloth.cmp(10, 2) > 0);
+        test.ok(sloth.cmp(2, 2) == 0);
+        test.done();
+    },
+
+    testIterRange: function(test) {
+        test.expect(3);
+        var iter = sloth.iterRange(1, 5, 2);
+        test.strictEqual(1, iter());
+        test.strictEqual(3, iter());
+        try {
+            iter();
+        } catch(e) {
+            test.strictEqual(sloth.StopIteration, e);
+        }
+        test.done();
+    },
+
+    testIterRangeDefaultStep: function(test) {
+        test.expect(5);
+        var iter = sloth.iterRange(1, 5);
+        test.strictEqual(1, iter());
+        test.strictEqual(2, iter());
+        test.strictEqual(3, iter());
+        test.strictEqual(4, iter());
+        try {
+            iter();
+        } catch(e) {
+            test.strictEqual(sloth.StopIteration, e);
+        }
+        test.done();
+    },
+
+    testIterRangeOpen: function(test) {
+        test.expect(5);
+        var iter = sloth.iterRange(4);
+        test.strictEqual(0, iter());
+        test.strictEqual(1, iter());
+        test.strictEqual(2, iter());
+        test.strictEqual(3, iter());
+        try {
+            iter();
+        } catch(e) {
+            test.strictEqual(sloth.StopIteration, e);
+        }
+        test.done();
+    },
+
+    testCmpString: function(test) {
+        test.expect(3);
+        test.ok(sloth.cmp("t", "te") < 0);
+        test.ok(sloth.cmp("te", "t") > 0);
+        test.ok(sloth.cmp("t", "t") == 0);
+        test.done();
+    },
+
     testMap: function(test) {
         test.expect(5);
         var iter = sloth.wrap(sloth.iterArray([1, 2, 3, 4])).map(function(x) { return x + 1; }).next;
@@ -97,6 +171,36 @@ module.exports = {
         test.done();
     },
 
+    testSort: function(test) {
+        test.expect(5);
+        var iter = sloth.wrap(sloth.iterArray([1, 2, 10, 3])).sort().next;
+        test.strictEqual(1, iter());
+        test.strictEqual(2, iter());
+        test.strictEqual(3, iter());
+        test.strictEqual(10, iter());
+        try {
+            iter();
+        } catch(e) {
+            test.strictEqual(sloth.StopIteration, e);
+        }
+        test.done();
+    },
+
+    testSortCompareFunction: function(test) {
+        test.expect(5);
+        var iter = sloth.wrap(sloth.iterArray([1, 2, 3, 4])).sort(function(a, b) { return b - a; }).next;
+        test.strictEqual(4, iter());
+        test.strictEqual(3, iter());
+        test.strictEqual(2, iter());
+        test.strictEqual(1, iter());
+        try {
+            iter();
+        } catch(e) {
+            test.strictEqual(sloth.StopIteration, e);
+        }
+        test.done();
+    },
+
     testFoldl: function(test) {
         test.expect(1);
         test.strictEqual(-8, sloth.wrap(sloth.iterArray([1, 2, 3, 4])).foldl(function(acc, x) { return acc - x; }));
@@ -151,6 +255,12 @@ module.exports = {
         test.done();
     },
 
+    testAllPredicated: function(test) {
+        test.expect(1);
+        test.strictEqual(true, sloth.wrap(sloth.iterArray([1, 2, 3, 4])).any(function(x) { return x >= 2; }));
+        test.done();
+    },
+
     testAllFalse: function(test) {
         test.expect(1);
         test.strictEqual(false, sloth.wrap(sloth.iterArray([true, false, true, true])).all());
@@ -175,10 +285,110 @@ module.exports = {
         test.done();
     },
 
+    testAnyPredicated: function(test) {
+        test.expect(1);
+        test.strictEqual(true, sloth.wrap(sloth.iterArray([1, 2, 3, 4])).all(function(x) { return x >= 1; }));
+        test.done();
+    },
+
     testAnyInfinite: function(test) {
         test.expect(1);
         test.strictEqual(true, sloth.wrap(function() { return true; }).any());
         test.done();
-    }
+    },
+
+    testMax: function(test) {
+        test.expect(1);
+        test.strictEqual(4, sloth.wrap(sloth.iterArray([1, 2, 4, 3])).max());
+        test.done();
+    },
+
+    testMaxComparator: function(test) {
+        test.expect(1);
+        test.strictEqual(1, sloth.wrap(sloth.iterArray([1, 2, 4, 3])).max(function(a, b) { return b - a; }));
+        test.done();
+    },
+
+    testMin: function(test) {
+        test.expect(1);
+        test.strictEqual(1, sloth.wrap(sloth.iterArray([1, 2, 4, 3])).min());
+        test.done();
+    },
+
+    testMinComparator: function(test) {
+        test.expect(1);
+        test.strictEqual(4, sloth.wrap(sloth.iterArray([1, 2, 4, 3])).min(function(a, b) { return b - a; }));
+        test.done();
+    },
+
+    testTake: function(test) {
+        test.expect(5);
+        var iter = sloth.wrap((function() { var n = 1; return function() { return n++; }; })()).take(4).next;
+        test.strictEqual(1, iter());
+        test.strictEqual(2, iter());
+        test.strictEqual(3, iter());
+        test.strictEqual(4, iter());
+        try {
+            iter();
+        } catch(e) {
+            test.strictEqual(sloth.StopIteration, e);
+        }
+        test.done();
+    },
+
+    testSkip: function(test) {
+        test.expect(4);
+        var iter = sloth.wrap((function() { var n = 1; return function() { return n++; }; })()).skip(4).next;
+        test.strictEqual(5, iter());
+        test.strictEqual(6, iter());
+        test.strictEqual(7, iter());
+        test.strictEqual(8, iter());
+        test.done();
+    },
+
+    testTakeWhile: function(test) {
+        test.expect(5);
+        var iter = sloth.wrap(sloth.iterArray([1, 2, 3, 4, 5, 1])).takeWhile(function(x) { return x < 5 }).next;
+        test.strictEqual(1, iter());
+        test.strictEqual(2, iter());
+        test.strictEqual(3, iter());
+        test.strictEqual(4, iter());
+        try {
+            iter();
+        } catch(e) {
+            test.strictEqual(sloth.StopIteration, e);
+        }
+        test.done();
+    },
+
+    testSkipWhile: function(test) {
+        test.expect(5);
+        var iter = sloth.wrap(sloth.iterArray([4, 5, 6, 7, 4])).skipWhile(function(x) { return x < 5 }).next;
+        test.strictEqual(5, iter());
+        test.strictEqual(6, iter());
+        test.strictEqual(7, iter());
+        test.strictEqual(4, iter());
+        try {
+            iter();
+        } catch(e) {
+            test.strictEqual(sloth.StopIteration, e);
+        }
+        test.done();
+    },
+
+    testConcat: function(test) {
+        test.expect(5);
+        var iter = sloth.wrap(sloth.iterArray([1, 2])).concat(sloth.iterArray([3, 4])).next;
+        test.strictEqual(1, iter());
+        test.strictEqual(2, iter());
+        test.strictEqual(3, iter());
+        test.strictEqual(4, iter());
+        try {
+            iter();
+        } catch(e) {
+            test.strictEqual(sloth.StopIteration, e);
+        }
+        test.done();
+    },
 };
 
